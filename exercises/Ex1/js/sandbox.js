dac8453bfea945926210882840c29d9fb0ae1164
abhishejain16 <abhishek.jain@vinsol.com>
2012-07-30 16:38:46 +0530

    first

---
+++ exercises/Ex1/js/sandbox.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+console.log($('div.module'));
+console.log($('ul#myList li').eq(2));
+console.log($('ul#myList li#myListItem'));
+console.log($('li#myListItem'));
+console.log($('label[for=q]'));
+console.log($(':hidden').length);
+console.log($('img[alt]').length);
+console.log($('tr:odd'));
+});
+
