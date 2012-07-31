764c3f58142518ef3fb6e785b52f51dfdf5d5986
abhishejain16 <abhishek.jain@vinsol.com>
2012-07-31 11:08:33 +0530

    inputhint

---
+++ exercises/Ex4/js/inputHint.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+var search = $('label[for=q]').html();
+$input=$('input[name=q]').attr({value:search,
+'class':'hint'
+});
+
+$('label[for=q]').remove();
+
+$input.focus(function() {
+    $input.attr({value:null,
+    'class':null
+    });
+});
+
+
+$input.blur(function() {    
+if(this.value == null || this.value == "")
+{
+    $input.attr({value:search,
+    'class':'hint'
+    });
+}
+});
+
+
+
+});
+
